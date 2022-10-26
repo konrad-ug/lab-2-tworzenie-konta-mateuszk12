e37fef1aee1218b7bf4a29b7298dd701a1f5d3e7
MateuszK2 <mateusz.krz314@gmail.com>
2022-10-26 11:12:46 +0200

    testy i konto

--- app/Konto.py
+++ app/Konto.py
@@ -1,6 +1,22 @@
 class Konto:
-    def __init__(self, imie, nazwisko):
+    def __init__(self, imie, nazwisko,pesel,kod=None):
         self.imie = imie
         self.nazwisko = nazwisko
         self.saldo = 0
+        self.pesel_check(pesel)
+        if kod != None:
+            self.kod_check(kod)
+    def pesel_check(self,pesel):
+        if len(pesel) == 11:
+            self.pesel = pesel
+        else:
+            self.pesel =  "Niepoprawny pesel!"
+    def kod_check(self,kod):
+        if kod[0:6] == "PROMO_" and len(kod[6:]) == 3:
+            self.kod = kod
+        else:
+            self.kod = None
+
+
+        
 
--- app/tests/test_create_bank_account.py
+++ app/tests/test_create_bank_account.py
@@ -5,9 +5,26 @@ from ..Konto import Konto
 class TestCreateBankAccount(unittest.TestCase):
 
     def test_tworzenie_konta(self):
-        pierwsze_konto = Konto("Dariusz", "Januszewski")
+        pierwsze_konto = Konto("Dariusz", "Januszewski","123456789098")
         self.assertEqual(pierwsze_konto.imie, "Dariusz", "Imie nie zostało zapisane!")
         self.assertEqual(pierwsze_konto.nazwisko, "Januszewski", "Nazwisko nie zostało zapisane!")
         self.assertEqual(pierwsze_konto.saldo, 0, "Saldo nie jest zerowe!")
-
     #tutaj proszę dodawać nowe testy
+    def test_pesel(self):
+        konto_pesel_poprawny = Konto("Jan","Kowalski","12345678900")
+        konto_pesel_bledny = Konto("Jan","Kowalski","132123123123123123132")
+        self.assertEqual(konto_pesel_poprawny.pesel,"12345678900","pesel nie został zapisany")
+        self.assertEqual(len(konto_pesel_poprawny.pesel),11,"niepoprawna dlugosc numeru  pesel")
+        self.assertEqual(konto_pesel_poprawny.pesel,"12345678900","niepoprawny pesel")
+        self.assertEqual(konto_pesel_bledny.pesel,"Niepoprawny pesel!","nie dziala sprawdzanie peselu")
+    def test_kod_promocyjny(self):
+        konto_poprawny_kod = Konto("Jan","Kowalski","12345678900","PROMO_123")
+        konto_bledny_kod1 = Konto("Jan","Kowalski","12345678900","PROM_1234")
+        konto_bledny_kod2 = Konto("Jan","Kowalski","12345678900","PROM_123")
+        self.assertEqual(konto_poprawny_kod.kod,"PROMO_123","blednie zapiany kod")
+        self.assertEqual(konto_poprawny_kod.kod[0:5],"PROMO","blednie zapiany kod")
+        self.assertEqual(konto_poprawny_kod.kod[5:9],"_123","blednie zapiany kod")
+        self.assertEqual(konto_poprawny_kod.kod,"PROMO_123","blednie zapiany kod")
+        self.assertEqual(len(konto_poprawny_kod.kod),9,"bledna dlugosc kodu")
+        self.assertEqual(konto_bledny_kod1.kod,None,"zły kod jest możliwy do wprowadzenia")
+        self.assertEqual(konto_bledny_kod2.kod,None,"zły kod jest możliwy do wprowadzenia")
